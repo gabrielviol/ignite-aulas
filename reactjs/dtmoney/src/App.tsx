025a662499d8f9f0039fd253ea9de206174103a5
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-05-21 15:56:15 -0300

    Instalando styled-components e aplicando

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257ea;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
