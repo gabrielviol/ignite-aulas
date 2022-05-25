9dcf2314e7a797901780cd78ec1a6c2fc25f6c97
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-05-25 20:46:15 -0300

    Listando transações e seeds

--- reactjs/dtmoney/src/index.tsx
+++ reactjs/dtmoney/src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model, 
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:45:20')
+        },
+        {
+          id: 2,
+          title: 'Alugel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 800,
+          createdAt: new Date('2022-07-09 19:05:29')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
@@ -19,7 +42,7 @@ createServer({
       const data = JSON.parse(request.requestBody)
 
       return schema.create('transation', data)
-      
+
     })
   }
 })
