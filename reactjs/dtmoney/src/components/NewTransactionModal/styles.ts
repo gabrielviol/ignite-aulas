095e2ec6b27e1e656be5708067c8acd446325ea0
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-05-25 20:02:59 -0300

    Salvando dados do Form

--- reactjs/dtmoney/src/components/NewTransactionModal/styles.ts
+++ reactjs/dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
 
-export const Container = styled.div`
+export const Container = styled.form`
     h2 {
         color: var(--text-title);
         font-size: 1.5rem;
@@ -58,14 +58,23 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
     isActive: boolean;
+    activeColor: 'green' | 'red';
 }
+
+const colors ={
+    green: '#33cc95',
+    red: '#E52E4D'
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
-    background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+    background: ${(props) => props.isActive 
+    ? transparentize(0.9, colors[props.activeColor])
+    : 'transparent'
+    };
 
     display: flex;
     align-items: center;
