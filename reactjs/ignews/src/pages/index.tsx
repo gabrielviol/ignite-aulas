45a52d38a1469e0bf6aaa15cd1f58549ab00e063
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-06-04 17:08:41 -0300

    Definindo estilos globais de cores

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 
-import styles from "../styles/home.module.scss"
-
 export default function Home() {
   return (
     <>
@@ -9,9 +7,9 @@ export default function Home() {
         <title>Início | ig.news</title>
       </Head>
 
-      <h1 className={styles.title}>
+      <h1>
 
-        <span>Hello</span> world
+        Hello world
       </h1>
     </>
   )
