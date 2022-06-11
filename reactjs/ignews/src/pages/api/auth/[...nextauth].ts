dfc5afd6f4aa227407627eedf0bd849714322f79
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-06-10 21:21:39 -0300

    Verificando duplicidade e valores no BD

--- reactjs/ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import { query as q } from 'faunadb'
- 
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
@@ -10,25 +10,41 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization: { params: {scope: 'read:user'}}
+      authorization: { params: { scope: 'read:user' } }
     }),
   ],
   callbacks: {
     async signIn({ user, account, profile }) {
       const { email } = user
 
-      try{
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: { email }}
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
         )
-      )
-      return true
+        return true
       } catch {
         return false
       }
-     
+
     }
   }
 })
