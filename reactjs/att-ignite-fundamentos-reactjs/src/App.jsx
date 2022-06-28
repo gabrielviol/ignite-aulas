d054349a08960c7d5e52054007a0d04868ea9fe5
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-06-28 11:24:40 -0300

    Conceitos sobre css module e aplicação

--- reactjs/att-ignite-fundamentos-reactjs/src/App.jsx
+++ reactjs/att-ignite-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post"
+
+import './styles.css';
 
 export function App() {
   return (
     <>
+      <Header />
       <Post
         author="Gabriel Viol"
         content="Que legal, aprendi!!!"
