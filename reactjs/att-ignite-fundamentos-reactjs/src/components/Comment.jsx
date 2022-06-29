1f3edd54428b801777c1a9e25d7f0b0cf0ea27d4
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-06-29 19:50:05 -0300

    Finalizando estilização do Comment e toda parte estrutural e css da aplicação

--- reactjs/att-ignite-fundamentos-reactjs/src/components/Comment.jsx
+++ reactjs/att-ignite-fundamentos-reactjs/src/components/Comment.jsx
@@ -15,7 +15,7 @@ export function Comment() {
                         </div>
 
                         <button title='Deletar comentário'>
-                            <Trash size={20}/>
+                            <Trash size={24}/>
                         </button>
                     </header>
                     <p>Muito bom Devon, parabéns!!</p>
