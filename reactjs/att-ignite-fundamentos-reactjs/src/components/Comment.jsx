3071ea1029d433f5cd527c1387f07dbdf4edfd85
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-06-29 19:35:24 -0300

    Criação do componente Comment para comentarios no Post

---
+++ reactjs/att-ignite-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return(
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/20782852?v=4"/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}> 
+                            <strong>Gabriel Oliveira</strong>
+                            <time title='29 de Junho às 16:24h' dateTime='2022-06-29 16:04:32'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
