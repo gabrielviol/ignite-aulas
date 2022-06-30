171016d5ad558c1dce5e5e8e922fe5bdf14d7ef5
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-06-30 15:30:11 -0300

    Iteração com os dados dos Posts

--- reactjs/att-ignite-fundamentos-reactjs/src/App.jsx
+++ reactjs/att-ignite-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 
 import './global.css';
 
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gabrielviol.png',
+      name: 'Gabriel Oliveira',
+      role: 'Desenvolvedor Web'
+    },
+    content: [
+      {type:'paragraph', content:'Fala galeraa 👋',},
+      {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      {type:'link', content:'jane.design/doctorcare',}
+    ],
+    publishedAt: new Date('2022-06-21 22:18:15'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      {type:'paragraph', content:'Fala galeraa 👋',},
+      {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      {type:'link', content:'jane.design/doctorcare',}
+    ],
+    publishedAt: new Date('2022-06-15 15:18:15'),
+  }
+]
+
 export function App() {
   return (
     <>
@@ -14,10 +45,17 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-          author="Gabriel Viol"
-          content="Que legal, aprendi!!!"
-          />
+          {postMessage.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })
+
+          }
         </main>
       </div>
     </>
