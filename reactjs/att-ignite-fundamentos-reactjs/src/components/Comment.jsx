fa2ed5f389b51cb369f86d352a65b1687521a9c0
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-07-01 14:04:10 -0300

    Inserindo comentarios com programação declarativa

--- reactjs/att-ignite-fundamentos-reactjs/src/components/Comment.jsx
+++ reactjs/att-ignite-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment() {
+export function Comment({content}) {
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/20782852?v=4"/>
@@ -19,7 +19,7 @@ export function Comment() {
                             <Trash size={24}/>
                         </button>
                     </header>
-                    <p>Muito bom Devon, parabéns!!</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
--- reactjs/att-ignite-fundamentos-reactjs/src/components/Post.jsx
+++ reactjs/att-ignite-fundamentos-reactjs/src/components/Post.jsx
@@ -9,9 +9,10 @@ import styles from './Post.module.css'
 
 export function Post({ author, publishedAt, content }) {
     const [comments, setComments] = useState([
-        1,
-        2,
+        'Post muito bacana, hein?!'
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -25,9 +26,14 @@ export function Post({ author, publishedAt, content }) {
     function handleCreateNewComment() {
         event.preventDefault()
 
-        setComments([...comments, comments.length + 1]);
+        const newCommentText = event.target.comment.value
 
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
 
+    function handleNewcommentChange() {
+        setNewCommentText(event.target.value);
     }
 
     return(
@@ -60,7 +66,10 @@ export function Post({ author, publishedAt, content }) {
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
+                    name='comment'
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewcommentChange}
                 />
 
                 <footer>
@@ -70,7 +79,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment content={comment}/>
                 })
 
                 }
