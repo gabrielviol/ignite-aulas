f9827d62b8b6dc3cae2c8262d2a2a76061050007
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-08-30 12:37:51 -0300

    Criando componentes Header e Summary

--- aulas/consumo-api-e-performance/03-dt-money/src/App.tsx
+++ aulas/consumo-api-e-performance/03-dt-money/src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -8,8 +9,11 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-    <h1>Hello world</h1>
+
+      <Transactions />
+
     </ThemeProvider>
+
   )
 }
 
