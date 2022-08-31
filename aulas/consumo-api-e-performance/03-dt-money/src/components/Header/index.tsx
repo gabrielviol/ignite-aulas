c803dc9a47065d284489f2105fbb3fe293d9da7c
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-08-31 13:23:00 -0300

    Adicionando radix para acessibilidade e criação do dialog

--- aulas/consumo-api-e-performance/03-dt-money/src/components/Header/index.tsx
+++ aulas/consumo-api-e-performance/03-dt-money/src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import logoImg from '../../assets/logo.svg'
+
 
 export function Header(){
     return(
@@ -8,7 +10,21 @@ export function Header(){
             <HeaderContent>
                 <img src={logoImg} alt="" />
 
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay/>
+
+                        <Dialog.Content>
+                            <Dialog.Title>Nova transação</Dialog.Title>
+
+                            <Dialog.Close/>
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
