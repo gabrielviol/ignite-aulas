37c6f1b1267b69bd9511d7d2ca217e400c5b8d86
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-09-02 13:02:55 -0300

    Usando reduce para calculo das transações

--- aulas/consumo-api-e-performance/03-dt-money/src/components/Summary/index.tsx
+++ aulas/consumo-api-e-performance/03-dt-money/src/components/Summary/index.tsx
@@ -6,6 +6,26 @@ import { TransactionsContext } from "../../contexts/TransactionsContext";
 export function Summary(){
     const { transactions } = useContext(TransactionsContext);
 
+    //reduce => { income: 0, outcome: 0, total: 0 }
+
+    const summary = transactions.reduce(
+        (acc, transaction) => {
+            if(transaction.type === 'income'){
+                acc.income += transaction.price;
+                acc.total += transaction.price;
+            } else {
+                acc.outcome += transaction.price;
+                acc.total -= transaction.price;
+            }
+
+            return acc;
+        }, 
+        { 
+            income: 0, 
+            outcome: 0, 
+            total: 0 
+        }
+    )
 
     return(
         <SummaryContainer>
@@ -14,7 +34,7 @@ export function Summary(){
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -22,7 +42,7 @@ export function Summary(){
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color="#f75a68"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
@@ -31,7 +51,7 @@ export function Summary(){
                     
                     <CurrencyDollar size={32} color="#fff"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
