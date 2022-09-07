469a4737bb61ba9fa4fdefa42d1e8a9a6ea6b63f
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-09-07 19:17:55 -0300

    Adicionando Eslint para padronização do codigo

--- aulas/consumo-api-e-performance/03-dt-money/src/App.tsx
+++ aulas/consumo-api-e-performance/03-dt-money/src/App.tsx
@@ -1,12 +1,10 @@
-import { ThemeProvider } from "styled-components";
-import { TransactionsProvider } from "./contexts/TransactionsContext";
-import { Transactions } from "./pages/Transactions";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { TransactionsProvider } from './contexts/TransactionsContext'
+import { Transactions } from './pages/Transactions'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
@@ -14,7 +12,5 @@ export function App() {
         <Transactions />
       </TransactionsProvider>
     </ThemeProvider>
-
   )
 }
-
