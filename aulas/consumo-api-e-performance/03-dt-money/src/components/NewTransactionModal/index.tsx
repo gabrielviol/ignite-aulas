4fdeafd0323a8879b334bb3f6cb2683a30b6c823
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-09-07 19:04:53 -0300

    Criando nova transação e atualizando array no server.json

--- aulas/consumo-api-e-performance/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ aulas/consumo-api-e-performance/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,8 @@ import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 const newTransactionFormSchema = z.object({
     description: z.string(),
@@ -16,19 +18,32 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+    const { createTransaction } = useContext(TransactionsContext)
+
     const {
         control,
         register,
         handleSubmit,
-        formState: { isSubmitting }
+        formState: { isSubmitting },
+        reset
     } = useForm<NewTransactionFormInputs>({
         resolver: zodResolver(newTransactionFormSchema),
+        defaultValues: {
+            type: 'income'
+        }
     })
 
     async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-        await new Promise(resolve => setTimeout(resolve, 2000));
-
-        console.log(data)
+        const { category, description, price, type } = data;
+        
+        await createTransaction({
+            description,
+            price,
+            category,
+            type
+        })
+        
+        reset();
     }
 
     return (
