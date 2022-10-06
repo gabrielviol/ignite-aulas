4d07f277b877f342131e93703838560a9f0704b6
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-10-06 07:45:44 -0300

    add images for create styled page

--- aulas/04-ignite-shop/src/pages/index.tsx
+++ aulas/04-ignite-shop/src/pages/index.tsx
@@ -1,11 +1,16 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat'
-})
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta2 from '../assets/camisetas/3.png';
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480}/>
+      </Product>
+    </HomeContainer>
   )
 }
