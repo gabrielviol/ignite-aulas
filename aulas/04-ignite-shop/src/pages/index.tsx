02f49a6fc0ebdbe7fa9054281a64070971209e3c
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-10-08 13:36:03 -0300

    pull params in stripe

--- aulas/04-ignite-shop/src/pages/index.tsx
+++ aulas/04-ignite-shop/src/pages/index.tsx
@@ -15,7 +15,7 @@ interface HomeProps {
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[]
 }
 
@@ -67,9 +67,6 @@ export const getStaticProps: GetStaticProps = async () => {
       }).format(price.unit_amount / 100),
     }
   })
-
-
-  console.log(response)
 
   return {
     props: {
